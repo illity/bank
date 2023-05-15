5f328e5a4f0226fbca02fa1cc9e916fb0f7a051f
illity <arthursbdantas@gmail.com>
2023-05-15 02:16:37 -0300

    modified main js script to not crash on non-existing elements, add SHA256 encryption in the serverside making the original password pass through two SHA256 to increase safety and added function to validate login

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const crypto = require('crypto')
 const { MongoClient } = require('mongodb');
 
 const hostname = '127.0.0.1';
@@ -12,9 +13,9 @@ const dbName = 'mydb';
 const server = http.createServer((request, response) => {
     request.on('data', (chunk) => {
         const data = chunk.toString();
-        newUser(dbUrl, JSON.parse(data))
-            .then(console.log)
-            .catch(console.error)
+        newUser(JSON.parse(data))
+            .then(result => console.log(result))
+            .catch(error => console.error(error))
             .finally(() => client.close());
     }).on('end', () => {
         response.statusCode = 200;
@@ -29,11 +30,35 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-async function newUser(url, data) {
-    console.log(data)
+async function newUser(data) {
     await client.connect();
     const db = client.db(dbName);
     const collection = db.collection('accounts');
     const result = await collection.insertOne(data)
+    return result
 }
 
+async function validateLogin(login, encryptedPass) {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('accounts');
+        const result = await collection.findOne(login)
+        if (result) {
+            console.log(result['encryptedPass'])
+            console.log(encryptedPass)
+        } else {
+            throw new Error('Login does not exist. Please check your username and try again.')
+        }
+    } catch (error) {
+        console.error("Error logging in: ", error.message)
+    }
+}
+
+async function sha256(message) {
+    const msgBuffer = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
